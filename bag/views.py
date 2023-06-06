f89203a390925bcedeb306a2c03b2364e263c0a2
Andre <sarreqiandrea@gmail.com>
2023-06-06 14:35:04 +0000

    Correcting type error

--- bag/views.py
+++ bag/views.py
@@ -36,13 +36,9 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
-    return redirect(redirect_url)
-
 
 def adjust_bag(request, item_id):
-    """ adjust the shopping bag """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -52,10 +48,10 @@ def adjust_bag(request, item_id):
 
     if size:
         if quantity > 0:
-            bag[item_id][items_by_size] = quantity
+            bag[item_id]['items_by_size'][size] = quantity
         else:
-            del bag[item_id][items_by_size]
-            if not bag[item_id][items_by_size]:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
     else:
         if quantity > 0:
@@ -68,18 +64,17 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ adjust the shopping bag """
-
-    quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
+    """Remove the item from the shopping bag"""
 
     try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
         if size:
-            del bag[item_id][items_by_size]
-            if not bag[item_id][items_by_size]:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
         else:
             bag.pop(item_id)
@@ -88,4 +83,4 @@ def remove_from_bag(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
-        return HttpResponse(status=200)
+        return HttpResponse(status=500)
