50b3151769987b1a536f652a5d5681109c315980
Andre <sarreqiandrea@gmail.com>
2023-06-07 14:05:26 +0000

    Added forms.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,28 +1,30 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
-# Register your models here.
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
-    readonly_fields = ('lineitem_total')
+    readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineItemAdminInline)
+    inlines = (OrderLineItemAdminInline,)
+
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total')
-    fields = ('order_number', 'date',
-              'full_name', 'email', 'phone_number',
-              'country', 'postcode', 'town_or_city',
-              'street_address1', 'street_address2', 'county',
-              'delivery_cost', 'order_total',
-              'grand_total')
-    list_display = ('order_number', 'date',
-                    'full_name', 'delivery_cost',
-                    'order_total', 'grand_total')
-    order = ('date',)
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
 
 
-admin.site.register(order, OrderAdmin)
+admin.site.register(Order, OrderAdmin)
