2dc8f216663922b095079da6634868d49ccc3d6d
Andre <sarreqiandrea@gmail.com>
2023-06-12 15:44:39 +0000

    Adding payment functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n(x!!epnrizbu8)hp)wi)((5am#+rr0#6&4cllhpn1babjqans
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-andreasarre-boutiqueado-09ek4db71d9.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['8000-andreasarre-boutiqueado-cux7fi2s808.ws-eu99.gitpod.io']
 
 
 # Application definition
@@ -170,3 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIP{E_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIP{E_SECRET_KEY', '')
