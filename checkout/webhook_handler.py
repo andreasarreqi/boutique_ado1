2cff55d46f50a836720330f56dcbf5d7343e0a3c
Andre <sarreqiandrea@gmail.com>
2023-06-16 19:29:09 +0000

    Added webhook

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -14,3 +14,19 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle a handle_payment_intent_succeeded event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle handle_payment_intent_payment_failed from stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
